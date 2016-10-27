dbe23faf9f3217dad6056e3c3876ab6c3c51d77d
Chun Rong <crphang@gmail.com>
2016-10-27 18:11:39 +0800

    Catching Exceptions

--- crawler.py
+++ crawler.py
@@ -49,63 +49,71 @@ def getLinks_ebay(url, soup, protocol, domain):
                 links = links + [link]
     return links
 
+# Main Parse Function
 def parse(base_url, html_text):
     csv = "error"
     if ("amazon.com" in base_url):
         csv = parse_amazon(html_text)
     elif ("ebay.com" in base_url):
         csv = parse_ebay(html_text)
-    return base_url + "," + csv 
+    elif ("lazada.sg" in base_url):
+        csv = parse_lazada(html_text)
+    elif ("aliexpress.com" in base_url):
+        csv = parse_aliexpress(html_text)
+    return csv 
    
 #Input: HTML Text
 #Output: "url,name,image,price,mean" (if successful)
 #        "error" (if fail)         
 def parse_amazon(html_text):
-    soup = BeautifulSoup(html_text) # need to change
-    # find name
-    seivedMeta = soup.find_all('meta', attrs={'name':'title'})
-    text = str(seivedMeta[0])
-    text = text.replace(":", "|")
-    name = text.split('|')
-    name = name[1].strip()
+    try:
+        soup = BeautifulSoup(html_text) # need to change
+        # find name
+        seivedMeta = soup.find_all('meta', attrs={'name':'title'})
+        text = str(seivedMeta[0])
+        text = text.replace(":", "|")
+        name = text.split('|')
+        name = name[1].strip()
 
-    # find image
-    seivedImage = soup.findAll('img', {"data-a-dynamic-image":True} ) 
-    img = seivedImage[0]['data-a-dynamic-image']
-    end = img.split('"')
-    img = end[1]
+        # find image
+        seivedImage = soup.findAll('img', {"data-a-dynamic-image":True} ) 
+        img = seivedImage[0]['data-a-dynamic-image']
+        end = img.split('"')
+        img = end[1]
 
-    # find price
-    seivedSpan =soup.find_all('span', attrs={'id':'miniATF_price'})
-    if(len(seivedSpan) == 0):
+        # find price
+        seivedSpan =soup.find_all('span', attrs={'id':'miniATF_price'})
+        if(len(seivedSpan) == 0):
+            return "error"
+
+        value=  str(seivedSpan[0].contents[0]).strip()
+        # remove dash if value is a range etc, $3.00 - $6.00
+        removedDash = value.split('-')
+
+        if(removedDash[0].find('$') == -1):
+            return "error"
+
+        # only 1 price, not a range
+        elif(len(removedDash) ==1):
+            firstVal = removedDash[0]
+            firstVal = (firstVal.split('$'))[1] # extract the numbers
+            mean = float(firstVal)
+            # return format = name, value, mean
+            # example: name, image, 43.00, 43.00        
+            return name + "," +img + "," + firstVal + " - " +firstVal + "," + firstVal
+
+        # range of prices
+        else:
+            firstVal = removedDash[0].strip()
+            secondVal = removedDash[1].strip()
+            firstVal = (firstVal.split('$'))[1]
+            secondVal = (secondVal.split('$'))[1]
+            mean = (float(firstVal)+float(secondVal))/2.0
+            # return format = name, range, mean
+            # return example: name, image, 40.00 - 60.00, 50.00
+            return name +"," + img + "," + firstVal + "-" + secondVal + "," + str(mean)
+    except:
         return "error"
-
-    value=  str(seivedSpan[0].contents[0]).strip()
-    # remove dash if value is a range etc, $3.00 - $6.00
-    removedDash = value.split('-')
-
-    if(removedDash[0].find('$') == -1):
-        return "error"
-
-    # only 1 price, not a range
-    elif(len(removedDash) ==1):
-        firstVal = removedDash[0]
-        firstVal = (firstVal.split('$'))[1] # extract the numbers
-        mean = float(firstVal)
-        # return format = name, value, mean
-        # example: name, image, 43.00, 43.00        
-        return name + "," +img + "," + firstVal + "," + firstVal
-
-    # range of prices
-    else:
-        firstVal = removedDash[0].strip()
-        secondVal = removedDash[1].strip()
-        firstVal = (firstVal.split('$'))[1]
-        secondVal = (secondVal.split('$'))[1]
-        mean = (float(firstVal)+float(secondVal))/2.0
-        # return format = name, range, mean
-        # return example: name, image, 40.00 - 60.00, 50.00
-        return name +"," + img + "," + firstVal + "-" + secondVal + "," + mean
 
 #Input: HTML Text
 #Output: "url,name,image,price,mean" (if successful)
@@ -113,25 +121,28 @@ def parse_amazon(html_text):
 
 #find title
 def parse_lazada(html_text):
-    seivedMeta = soup.find_all('title')
-    name = str(seivedMeta).split(">")[1]
-    name = name.split("|")[0].strip()
+    try:
+        seivedMeta = soup.find_all('title')
+        name = str(seivedMeta).split(">")[1]
+        name = name.split("|")[0].strip()
 
-#find image
-    seivedMeta = soup.find_all('meta', attrs={'itemprop':'image'})
-    lastMeta = None
-    for lastMeta in seivedMeta:pass
-    if lastMeta:
-        image = lastMeta
-    image = str(image).split("\"")[1]
+    #find image
+        seivedMeta = soup.find_all('meta', attrs={'itemprop':'image'})
+        lastMeta = None
+        for lastMeta in seivedMeta:pass
+        if lastMeta:
+            image = lastMeta
+        image = str(image).split("\"")[1]
 
-    #find price
-    seivedSpan = soup.find_all('span', attrs={'id':'special_price_box'})
-    if(len(seivedSpan) == 0):
-        print "error: no price found"
-    price = str(seivedSpan[0].contents[0])
-        
-    return name +"," + image + "," + price + "," + price
+        #find price
+        seivedSpan = soup.find_all('span', attrs={'id':'special_price_box'})
+        if(len(seivedSpan) == 0):
+            print "error: no price found"
+        price = str(seivedSpan[0].contents[0])
+            
+        return name +"," + image + "," + str(price) + " - " + str(price) + "," + str(price)
+    except:
+        return "error"
 
 
 
@@ -139,32 +150,39 @@ def parse_lazada(html_text):
 #Output: "url,name,price" (if successful)
 #        "error" (if fail) 
 def parse_ebay(html_text):
-    soup = BeautifulSoup(html_text)
-    span = soup.find_all('span', attrs={'id':'prcIsum'})[0]
-    name = soup.find_all('h1', attrs={'id':'itemTitle'})[0]
-    img = soup.findAll('img', {'id':'icImg'})[0]
-    if (len(name.contents) < 2 or len(span.contents) < 1):
-        return "error"
-    return name.contents[1] + "," + img.get('src') + "," + span.contents[0] + "-" + span.contents[0] + "," + span.contents[0]
+    try:
+        soup = BeautifulSoup(html_text)
+        span = soup.find_all('span', attrs={'id':'prcIsum'})[0]
+        print span
+        name = soup.find_all('h1', attrs={'id':'itemTitle'})[0]
+        img = soup.findAll('img', {'id':'icImg'})[0]
+        if (len(name.contents) < 2 or len(span.contents) < 1):
+            return "error"
+        return name.contents[1] + "," + img.get('src') + "," + str(span.contents[0]) + "-" + str(span.contents[0]) + "," + str(span.contents[0].split("$")[1])
+    except:
+        return "errror"
 
 #Input: HTML Text
 #Output: "url,name,price" (if successful)
 #        "error" (if fail) 
 def parse_aliexpress(html_text):
-    soup = BeautifulSoup(open("Untitled Document 3"))
-    tempPrice = soup.find_all('span', attrs={'itemprop':'lowPrice'})
-    if (len(tempPrice)== 0):
-        lowPrice = soup.find_all('span', attrs={'itemprop':'price'})[0]
-        highPrice = lowPrice            
-    else:
-        highPrice = soup.find_all('span', attrs={'itemprop':'highPrice'})[0]
-        lowPrice = tempPrice[0]
-    name = soup.find_all('h1', attrs={'itemprop':'name'})[0]
-    img = soup.findAll('img', {'alt':name.contents[0]})[0]
-    mean = (float(lowPrice.contents[0]) + float(highPrice.contents[0])) / 2.0
-    if (len(name.contents) < 1 or lowPrice.contents[0] < 0 or highPrice.contents[0] < 0):
-        print "error"
-    return name.contents[0] + "," + img.get('src') + "," + lowPrice.contents[0] + "-" + highPrice.contents[0] + "," + mean
+    try:
+        soup = BeautifulSoup(open("Untitled Document 3"))
+        tempPrice = soup.find_all('span', attrs={'itemprop':'lowPrice'})
+        if (len(tempPrice)== 0):
+            lowPrice = soup.find_all('span', attrs={'itemprop':'price'})[0]
+            highPrice = lowPrice            
+        else:
+            highPrice = soup.find_all('span', attrs={'itemprop':'highPrice'})[0]
+            lowPrice = tempPrice[0]
+        name = soup.find_all('h1', attrs={'itemprop':'name'})[0]
+        img = soup.findAll('img', {'alt':name.contents[0]})[0]
+        mean = (float(lowPrice.contents[0]) + float(highPrice.contents[0])) / 2.0
+        if (len(name.contents) < 1 or lowPrice.contents[0] < 0 or highPrice.contents[0] < 0):
+            print "error"
+        return name.contents[0] + "," + img.get('src') + "," + str(lowPrice.contents[0]) + "-" + str(highPrice.contents[0]) + "," + str(mean)
+    except:
+        return "error"
 
 
 # Main Program
@@ -207,8 +225,9 @@ if __name__ == "__main__":
             # print("Visiting: " + url)
             if ("page" not in url and "_pgn=" not in url and url != amazon_URL and url != ebay_URL): #product page
                 data = parse(url.encode('utf-8'), html_text.encode('utf-8'))
-                writer.writerow(data.split(','))
-                print(html_text)
+                if data != "error":
+                    data = url.encode('utf-8') + "," + data 
+                    writer.writerow(data.split(','))
 
             else: #Scrape
                 #print(soup.prettify())
